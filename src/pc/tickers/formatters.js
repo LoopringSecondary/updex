d449327d780f6f50ec33bb7053027097eb0e3f87
dyfvicture <fbi-ceo@163.com>
2018-10-09 17:31:24 +0800

    ticker search

--- src/pc/tickers/formatters.js
+++ src/pc/tickers/formatters.js
@@ -6,3 +6,19 @@ export const getMarketTickersBySymbol = (symbol,tickers)=>{
     return tokens.right === symbol
   })
 }
+
+export const searchMarkets = (symbol,tickers)=>{
+  return tickers.filter(ticker=>{
+    const tokens = getTokensByMarket(ticker.market)
+    if(symbol.indexOf('/') > -1) {
+      const searchMarket = symbol.split('/')
+      if(searchMarket[1]) {
+        return tokens.left === searchMarket[0].toUpperCase() && tokens.right.startsWith(searchMarket[1].toUpperCase())
+      } else {
+        return tokens.left === searchMarket[0].toUpperCase()
+      }
+    } else {
+      return tokens.right.indexOf(symbol.toUpperCase()) > -1 || tokens.left.indexOf(symbol.toUpperCase()) > -1
+    }
+  })
+}
