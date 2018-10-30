b005786dc5b7738dfad7c30a4072f90d0cde43dc
dyfvicture <fbi-ceo@163.com>
2018-10-30 15:51:37 +0800

    helper

--- src/face2face/HelperOfPrice.js
+++ src/face2face/HelperOfPrice.js
@@ -4,7 +4,10 @@ import { Button,NavBar } from 'antd-mobile';
 import { Icon } from 'antd';
 
 const HelperOfPrice = (props)=>{
-  const {dispatch} = props
+  const {p2pOrder, dispatch} = props
+  const {amountB, amountS, tokenS, tokenB} = p2pOrder
+
+  console.log(1111, amountB, amountS, tokenS, tokenB)
   return (
     <div className="fs20 bg-white" style={{height:"100%",overflow:'auto' }}>
       <NavBar
@@ -18,13 +21,13 @@ const HelperOfPrice = (props)=>{
         <div className="color-black-1">Price Helper</div>
       </NavBar>
       <div className="zb-b-b p15">
-        <div className="fs14 color-black-2 mb5 text-left">卖出LRC价格</div>
+        <div className="fs14 color-black-2 mb5 text-left">卖出{tokenS}价格</div>
         <div className="fs12 color-black-3 lh20 text-left">
-          1 LRC ≈ xxxx WETH ≈ $1.0
+          1 {tokenS} ≈ xxxx {tokenB} ≈ $1.0
         </div>
       </div>
       <div className="zb-b-b p10">
-        <div className="fs14 color-black-2 mb5 text-left">买入WETH价格</div>
+        <div className="fs14 color-black-2 mb5 text-left">买入{tokenB}价格</div>
         <div className="fs12 color-black-3 lh20 text-left">
           1 WETH ≈ xxxx LRC ≈ $200.0
         </div>
@@ -32,7 +35,11 @@ const HelperOfPrice = (props)=>{
     </div>
   )
 }
-export default connect()(HelperOfPrice)
+export default connect(({
+  p2pOrder
+}) => ({
+  p2pOrder:p2pOrder
+}))(HelperOfPrice)
 
 
 
