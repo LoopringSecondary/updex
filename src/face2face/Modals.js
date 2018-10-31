91716369636f2d3ac91afbfa056880420492dea8
hzbdesigner <hzbdesigner@gmail.com>
2018-10-31 21:42:03 +0800

    update p2p

--- src/face2face/Modals.js
+++ src/face2face/Modals.js
@@ -8,7 +8,6 @@ import HelperOfAmount from './HelperOfAmount'
 import HelperOfFAQ from './HelperOfFAQ'
 import HelperOfPrice from './HelperOfPrice'
 import Face2FaceConfirm from './Face2FaceConfirm'
-import Settings from './Settings'
 import OrderDetail from './Detail'
 import OrderQrcode from './Qrcode'
 import TakerConfirm from './TakerConfirm'
@@ -72,46 +71,6 @@ function Modals(props) {
         <UiContainers.Drawer id="helperOfAmountOfP2P" alias="helperOfAmount" position="right" className="h-100" style={{height:'100%',width:"48rem",margin:'0 auto'}}>
           <HelperOfAmount />
         </UiContainers.Drawer>
-      </Containers.Layers>
-      <Containers.Layers id="takerConfirm">
-        <UiContainers.Popups id="takerConfirm" className="h-100" style={{height:'100%'}} >
-          <TakerConfirm />
-        </UiContainers.Popups>
-      </Containers.Layers>
-      <Containers.Layers id="orderQrcode">
-        <UiContainers.Modals id="orderQrcode" className="rs">
-          <OrderQrcode/>
-        </UiContainers.Modals>
-      </Containers.Layers>
-      <Containers.Layers id="helperOfTTL">
-        <UiContainers.Popups id="helperOfTTL" className="h-100" style={{height:'100%'}}>
-          <HelperOfTTL />
-        </UiContainers.Popups>
-      </Containers.Layers>
-      <Containers.Layers id="helperOfMiniFill">
-        <UiContainers.Popups id="helperOfMiniFill" className="h-100" style={{height:'100%'}}>
-          <HelperOfMiniFill />
-        </UiContainers.Popups>
-      </Containers.Layers>
-      <Containers.Layers id="helperOfFAQ">
-        <UiContainers.Popups id="helperOfFAQ" className="h-100" style={{height:'100%'}}>
-          <HelperOfFAQ />
-        </UiContainers.Popups>
-      </Containers.Layers>
-      <Containers.Layers id="settings">
-        <UiContainers.Popups id="settings" className="h-100" style={{height:'100%'}}>
-          <Settings />
-        </UiContainers.Popups>
-      </Containers.Layers>
-      <Containers.Layers id="helperOfPrice">
-        <UiContainers.Popups id="helperOfPrice" className="h-100" style={{height:'100%'}}>
-          <HelperOfPrice />
-        </UiContainers.Popups>
-      </Containers.Layers>
-      <Containers.Layers id="helperOfAmount">
-        <UiContainers.Popups id="helperOfAmount">
-          <HelperOfAmount />
-        </UiContainers.Popups>
       </Containers.Layers>
     </div>
   )
