c2bef73b23a4fbfb80dbd05cd679f37bd9c9688d
lyd <lyddream@126.com>
2018-10-31 18:02:11 +0800

    fix p2p taker

--- src/face2face/TakerConfirm.js
+++ src/face2face/TakerConfirm.js
@@ -51,13 +51,13 @@ class TakerConfirm extends React.Component {
       amountS = toBig(makerOrder.originalOrder.amountB).idiv(makerOrder.count).plus(1)
     }
     order.amountS = toHex(amountS)
-    order.lrcFee = '0x0'
-    order.validSince = toHex(Math.ceil(validSince.valueOf() / 1e3))
-    order.validUntil = toHex(Math.ceil(validUntil.valueOf() / 1e3))
-    order.marginSplitPercentage = 0
-    order.buyNoMoreThanAmountB = true
+    order.lrcFee = '0x0';
+    order.validSince = toHex(Math.ceil(validSince.valueOf() / 1e3));
+    order.validUntil = toHex(Math.ceil(validUntil.valueOf() / 1e3));
+    order.marginSplitPercentage = 0;
+    order.buyNoMoreThanAmountB = true;
     order.walletAddress = (window.Wallet && window.Wallet.rewardAddress) || config.getWalletAddress()
-    const authAccount = createWallet()
+    const authAccount = createWallet();
     order.authAddr = authAccount.getAddressString();
     order.authPrivateKey = clearHexPrefix(authAccount.getPrivateKeyString());
     order.orderType = 'p2p_order'
@@ -65,16 +65,7 @@ class TakerConfirm extends React.Component {
     const completeOrder = {...order,tokenS :config.getTokenBySymbol(order.tokenS).address,tokenB:config.getTokenBySymbol(order.tokenB).address }
     const tokensFm = new TokenFm({symbol:order.tokenS})
     const tokenbFm = new TokenFm({symbol:order.tokenB})
-
     const price = toFixed(order.amountS / order.amountB, 4);
-    const showLayer = (payload = {}) => {
-      dispatch({
-        type: 'layers/showLayer',
-        payload: {
-          ...payload
-        }
-      })
-    }
     const hideLayer = (payload = {}) => {
       dispatch({
         type: 'layers/hideLayer',
@@ -87,16 +78,16 @@ class TakerConfirm extends React.Component {
     const submitRing = async () => {
 
       if(!socket){
-        Notification.open({description:'Please wait for loading data'});
+        Notification.open({description:'Please wait for loading data',type: 'error'});
         return
       }
       const address = (window.Wallet && window.Wallet.address) || storage.wallet.getUnlockedAddress()
-      const gasPrice = toHex(toBig(gas.tabSelected === 'estimate' ? gas.gasPrice.estimate : gas.gasPrice.current))
+      const gasPrice = toHex(toBig(gas.tabSelected === 'estimate' ? gas.gasPrice.estimate : gas.gasPrice.current).times(1e9))
       const tradeInfo = {...order,marginSplit:0,roleType:'taker',validSince:validSince.unix(),validUntil:validUntil.unix(),milliLrcFee:0,amountS:tokensFm.getUnitAmount(order.amountS),amountB:tokenbFm.getUnitAmount(order.amountB),
         gasLimit:config.getGasLimitByType('approve').gasLimit,
-        gasPrice:toHex(toNumber(gasPrice)*1e9)}
+        gasPrice}
       try {
-        await orderFormatter.p2pVerification(balance, tradeInfo, pendingTx ? pendingTx.items : [], gasPrice)
+        await orderFormatter.p2pVerification(balance, tradeInfo, pendingTx ? pendingTx.items : [], toBig(gasPrice).div(1e9))
       } catch (e) {
         Notification.open({
           message: intl.get('notifications.title.place_order_failed'),
@@ -112,7 +103,7 @@ class TakerConfirm extends React.Component {
           if(item.value.symbol.toLowerCase() === 'eth'){
             Notification.open({
               message: intl.get('notifications.title.place_order_failed'),
-              description: intl.get('eth_is_required_when_place_order', {required: item.value.required}),
+              description: intl.get('notifications.message.eth_is_required_when_place_order', {required: item.value.required}),
               type: 'error',
               actions: (
                 <div>
@@ -130,16 +121,6 @@ class TakerConfirm extends React.Component {
               message: intl.get('notifications.title.place_order_failed'),
               description: intl.get('notifications.message.token_required_when_place_order', {required: item.value.required,token:item.value.symbol}),
               type: 'error',
-              actions: (
-                <div>
-                  <Button className="alert-btn mr5" onClick={() => dispatch({
-                    type: 'layers/showLayer',
-                    payload: {id: 'receiveToken', symbol: item.value.symbol}
-                  })}>
-                    {`${intl.get('actions.receive')} ${item.value.symbol}`}
-                  </Button>
-                </div>
-              )
             })
           }
         dispatch({type: 'p2pOrder/loadingChange', payload: {loading: false}})
@@ -156,10 +137,8 @@ class TakerConfirm extends React.Component {
           })
           return
         }
-        const signedOrder = {...completeOrder, ...signResult.result}
-        signedOrder.powNonce = 100
-        const txs = unsigned.filter(item => item.type === 'tx')
-        console.log(txs)
+        const signedOrder = {...completeOrder, ...signResult.result,powNonce : 100}
+        const txs = unsigned.filter(item => item.type === 'tx');
         eachOfLimit(txs, 1, async (item) => {
           signTx(item.data).then(res => {
             if (res.result) {
@@ -188,19 +167,17 @@ class TakerConfirm extends React.Component {
               gasLimit: config.getGasLimitByType('submitRing').gasLimit,
               chainId: config.getChainId(),
               to: order.protocol,
-              gasPrice:toHex(toNumber(gasPrice)*1e9),
-              nonce:nonce,
+              gasPrice,
+              nonce,
               data:Contracts.LoopringProtocol.encodeSubmitRing([{...signedOrder},{...makerOrder.originalOrder,
                 tokenS:config.getTokenBySymbol(makerOrder.originalOrder.tokenS).address,
                 tokenB:config.getTokenBySymbol(makerOrder.originalOrder.tokenB).address,
                 owner:makerOrder.originalOrder.address,marginSplitPercentage:toNumber(makerOrder.originalOrder.marginSplitPercentage)}],config.getWalletAddress())
           };
-            // console.log(JSON.stringify(tx))
-            window.RELAY.order.placeOrder({...signedOrder,authPrivateKey:''}).then(reponse=> {
+            window.RELAY.order.placeOrder({...signedOrder,authPrivateKey:''}).then(response=> {
               signTx(tx).then(res => {
                 if (res.result) {
-                  // console.log(res.result);
-                  window.RELAY.ring.submitRingForP2P({makerOrderHash:makerOrder.originalOrder.hash,rawTx:res.result,takerOrderHash:toHex(window.RELAY.order.getOrderHash(completeOrder))}).then(resp => {
+                  window.RELAY.ring.submitRingForP2P({makerOrderHash:makerOrder.originalOrder.hash,rawTx:res.result,takerOrderHash:response.result}).then(resp => {
                     if (resp.result) {
                       Toast.success(intl.get('notifications.title.submit_ring_suc'), 3, null, false)
                       hideLayer({id: 'takerConfirm'})
