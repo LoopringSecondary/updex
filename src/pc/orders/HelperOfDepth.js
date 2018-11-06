020bff4bcc130c5bf1c1e09685a4650d2ceef334
hzbdesigner <hzbdesigner@gmail.com>
2018-11-06 12:44:26 +0800

    fix bug for depth overlofw

--- src/pc/orders/HelperOfDepth.js
+++ src/pc/orders/HelperOfDepth.js
@@ -57,112 +57,76 @@ class HelperOfDepth extends React.Component {
 
     return (
       <div className="d-flex flex-column" style={{height: '100%'}}>
-        <div className="" style={{flex: 1, overflow: 'auto'}}>
-          <table className="w-100 fs12">
-            <thead>
-            <tr className="">
-              <th
-                className="zb-b-b text-left pl15 pr5 pt5 pb5 text-nowrap font-weight-normal color-black-4">{intl.get("common.price")}</th>
-              <th
-                className="zb-b-b text-right pl5 pr5 pt5 pb5 text-nowrap font-weight-normal color-black-4">{intl.get("common.amount")} {tokens.left}</th>
-              <th
-                className="zb-b-b text-right pl5 pr15 pt5 pb5 text-nowrap font-weight-normal color-black-4">{intl.get("common.total")} {tokens.right}</th>
-            </tr>
-            </thead>
-            <tbody className="text-number">
-            <tr><td className="pt5"></td></tr>
+        <div className="fs12 position-relative h-100 d-flex flex-column" style={{height:'100%'}}>
+          <div className="row no-gutters ml0 mr0 w-100 bg-white" style={{position:'absolute',zIndex:1}}>
+            <div style={{width:'40%'}} className="col-auto zb-b-b text-left pl15 pr5 pt5 pb5 font-weight-normal color-black-4">{intl.get("common.price")} </div>
+            <div style={{width:'30%'}} className="col-auto zb-b-b text-right pl5 pr5 pt5 pb5 font-weight-normal color-black-4">{intl.get("common.amount")} {tokens.left}</div>
+            <div hidden className="col-3 zb-b-b text-right pl5 pr5 pt5 pb5 font-weight-normal color-black-4">{intl.get("common.LRCFee")} LRC</div>
+            <div style={{width:'30%'}} className="col-auto zb-b-b text-right pl5 pr15 pt5 pb5 font-weight-normal color-black-4">{intl.get("common.time")}</div>
+          </div>
+          <div className="pt30"></div>
+          <div className="text-number pt5 pb5 position-relative" style={{flex:1,zIndex:0,overflowY:'auto'}} refs="sellEnd">
+              <div className="d-flex flex-column justify-content-end" style={{minHeight:'100%'}}>
+                {
+                  sell && sell.reverse().map((item, index) =>
+                    <div key={index} className="row no-gutters ml0 mr0 w-100">
+                      <div style={{width:'40%'}} className="hover-default pl15 pr5 text-left color-error align-top lh20"
+                          onClick={changePrice.bind(this, Number(item[0]).toFixed(8))}>
+                        {Number(item[0]).toFixed(8)}
+                        <span className="fs12 color-black-4 ml5"><Worth amount={Number(item[0]).toFixed(8)}
+                                                                        symbol={tokens.right}/></span>
+                      </div>
+                      <div style={{width:'30%'}} className="hover-default pl5 pr5 color-black-2 text-right lh20"
+                          onClick={changeAmount.bind(this, Number(item[1]).toFixed(4))}>
+                        {Number(item[1]).toFixed(4)}
+                      </div>
+                      <div style={{width:'30%'}} className="hover-default pl5 pr15 color-black-2 text-right align-top lh20"
+                          onClick={changeAmount.bind(this, Number(item[1]).toFixed(4))}>
+                        {Number(item[2]).toFixed(4)}
+                      </div>
+                    </div>
+                  )
+                }
+              </div>
+          </div>
+          <div className="zb-b-t">
             {
-              sell && sell.reverse().map((item, index) =>
-                <tr key={index}>
-                  <td className="hover-default pl15 pr5 text-left color-error align-top lh20"
-                      onClick={changePrice.bind(this, Number(item[0]).toFixed(8))}>
-                    {Number(item[0]).toFixed(8)}
-                    <span className="fs12 color-black-4 ml5"><Worth amount={Number(item[0]).toFixed(8)}
-                                                                    symbol={tokens.right}/></span>
-                  </td>
-                  <td className="hover-default pl5 pr5 color-black-2 text-right lh20"
-                      onClick={changeAmount.bind(this, Number(item[1]).toFixed(4))}>
-                    {Number(item[1]).toFixed(4)}
-                  </td>
-                  <td className="hover-default pl5 pr15 color-black-2 text-right align-top lh20"
-                      onClick={changeAmount.bind(this, Number(item[1]).toFixed(4))}>
-                    {Number(item[2]).toFixed(4)}
-                  </td>
-                </tr>
-              )
+              trades.items.length > 0 && !isIncrease() && <div className="text-left pl15 pr15 pt10 pb10 zb-b-b">
+                <span className="color-error fs14" onClick={changePrice.bind(this, Number(trades.items[0].price).toFixed(8))}>{trades.items[0].price}</span>
+                <span className="color-black-4 fs12 ml10">≈ <Worth amount={Number(trades.items[0].price).toFixed(8)}
+                                                                   symbol={tokens.right}/></span>
+              </div>
             }
-            <tr ref='sellEnd'><td className="pt5"></td></tr>
             {
-              depth.item && depth.item.sell && depth.item.sell.length === 0 &&
-              <tr className="">
-                <td colSpan="10" className="p10 text-center align-top color-black-4 fs12">
-                  {intl.get('common.list.no_data_custom', {title: intl.get('common.depth')})}
-                </td>
-              </tr>
+              trades.items.length > 0 && isIncrease() && <div className="text-left pl15 pr15 pt10 pb10 zb-b-b">
+                <span className="color-success fs14" onClick={changePrice.bind(this, Number(trades.items[0].price).toFixed(8))}>{trades.items[0].price}</span>
+                <span className="color-black-4 fs12 ml10">≈ <Worth amount={Number(trades.items[0].price).toFixed(8)}
+                                                                   symbol={tokens.right}/></span>
+              </div>
             }
-            </tbody>
-          </table>
-        </div>
-        <div className="zb-b-t">
-          {
-            trades.items.length > 0 && !isIncrease() && <div className="text-left pl15 pr15 pt10 pb10 zb-b-b">
-              <span className="color-error fs14" onClick={changePrice.bind(this, Number(trades.items[0].price).toFixed(8))}>{trades.items[0].price}</span>
-              <span className="color-black-4 fs12 ml10">≈ <Worth amount={Number(trades.items[0].price).toFixed(8)}
-                                                                 symbol={tokens.right}/></span>
-            </div>
-          }
-          {
-            trades.items.length > 0 && isIncrease() && <div className="text-left pl15 pr15 pt10 pb10 zb-b-b">
-              <span className="color-success fs14" onClick={changePrice.bind(this, Number(trades.items[0].price).toFixed(8))}>{trades.items[0].price}</span>
-              <span className="color-black-4 fs12 ml10">≈ <Worth amount={Number(trades.items[0].price).toFixed(8)}
-                                                                 symbol={tokens.right}/></span>
-            </div>
-          }
-        </div>
-        <div className="" style={{flex: "1", overflow: 'auto'}}>
-          <table className="w-100 fs12">
-            <thead style={{opacity:'0',lineHeight:'0rem'}}>
-            <tr className="">
-              <th style={{lineHeight:'0rem'}}
-                  className="zb-b-b text-left pl15 pr5 text-nowrap font-weight-normal color-black-4">{intl.get("common.price")}</th>
-              <th style={{lineHeight:'0rem'}}
-                  className="zb-b-b text-right pl5 pr5 text-nowrap font-weight-normal color-black-4">{intl.get("common.amount")} {tokens.left}</th>
-              <th style={{lineHeight:'0rem'}}
-                  className="zb-b-b text-right pl5 pr15 text-nowrap font-weight-normal color-black-4">{intl.get("common.total")} {tokens.right}</th>
-            </tr>
-            </thead>
-            <tbody className="text-number">
-            <tr><td className="pt5"></td></tr>
+          </div>
+          <div className="text-number pt5 pb5 position-relative" style={{flex:1,overflow:'auto',zIndex:0}}>
             {
               depth.item && depth.item.buy && depth.item.buy.map((item, index) =>
-                <tr key={index}>
-                  <td className="hover-default pl15 pr5 text-left color-success align-top lh20"
+                <div key={index} className="row no-gutters ml0 mr0 w-100">
+                  <div style={{width:'40%'}} className="hover-default pl15 pr5 text-left color-success align-top lh20"
                       onClick={changePrice.bind(this, Number(item[0]).toFixed(8))}>
                     {Number(item[0]).toFixed(8)}
                     <span className="fs12 color-black-4 ml5"><Worth amount={Number(item[0]).toFixed(8)}
                                                                     symbol={tokens.right}/></span>
-                  </td>
-                  <td className="hover-default pl5 pr5 color-black-2 text-right align-top lh20"
+                  </div>
+                  <div style={{width:'30%'}} className="hover-default pl5 pr5 color-black-2 text-right lh20"
                       onClick={changeAmount.bind(this, Number(item[1]).toFixed(4))}>
                     {Number(item[1]).toFixed(4)}
-                  </td>
-                  <td className="hover-default pl5 pr15 color-black-2 text-right align-top lh20"
+                  </div>
+                  <div style={{width:'30%'}} className="hover-default pl5 pr15 color-black-2 text-right align-top lh20"
                       onClick={changeAmount.bind(this, Number(item[1]).toFixed(4))}>
                     {Number(item[2]).toFixed(4)}
-                  </td>
-                </tr>
+                  </div>
+                </div>
               )
             }
-            {
-              depth.item && depth.item.buy && depth.item.buy.length === 0 &&
-              <tr>
-                <td colSpan="10" className="p10 text-center align-top color-black-4 fs12">
-                  {intl.get('common.list.no_data_custom', {title: intl.get('common.depth')})}
-                </td>
-              </tr>
-            }
-            </tbody>
-          </table>
+          </div>
         </div>
       </div>
     )
