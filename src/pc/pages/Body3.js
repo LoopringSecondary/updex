481cdc8a640db00007875896bf69b1d44d42db06
hzbdesigner <hzbdesigner@gmail.com>
2018-11-07 10:25:47 +0800

    fix bug for layout3

--- src/pc/pages/Body3.js
+++ src/pc/pages/Body3.js
@@ -30,9 +30,29 @@ class Home extends React.Component {
               <PanelHeader title={intl.get('pc_panels.charts')} />
               <Kline />
             </PanelWrapper>
-            <div className="row no-gutters ml0 mr0 mtpanel" style={{height:'320px'}}>
+          </div>
+          <div className="col-auto d-flex flex-column mlpanel mtpanel">
+            <div className="row no-gutters ml0 mr0" style={{flex:1}}>
               <div className="col-6" style={{height:'100%'}}>
-                <PanelWrapper className="mlpanel" style={{flex:'1',height:'100%'}}>
+                <PanelWrapper style={{height:'100%',paddingBottom:'0.7rem',width:'25rem'}} className="">
+                  <PanelHeader title={intl.get('pc_panels.order_book')}  />
+                  <HelperOfDepth />
+                </PanelWrapper>
+              </div>
+              <div className="col-6" style={{height:'100%'}}>
+                <PanelWrapper style={{height:'100%',paddingBottom:'0.5rem',width:'25rem'}} className="mlpanel">
+                  <PanelHeader title={intl.get('pc_panels.trade_history')} />
+                  <ListMarketFills />
+                </PanelWrapper>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="d-flex w-100 align-items-stretch mtpanel">
+          <div className="" style={{flex:1}}>
+            <div className="row no-gutters ml0 mr0" style={{height:'100%'}}>
+              <div className="col-6" style={{height:'100%'}}>
+                <PanelWrapper className="mlpanel" style={{height:'100%'}}>
                   <PanelHeader title={intl.get('pc_panels.my_orders')} />
                   <div style={{flex:'1',overflow:'auto'}}>
                     <HelperOfMyMarketOrders />
@@ -40,7 +60,7 @@ class Home extends React.Component {
                 </PanelWrapper>
               </div>
               <div className="col-6" style={{height:'100%'}}>
-                <PanelWrapper className="mlpanel" style={{flex:'1',height:'100%'}}>
+                <PanelWrapper className="mlpanel" style={{height:'100%'}}>
                   <PanelHeader title={intl.get('pc_panels.my_wallet')} />
                   <div style={{flex:'1',overflow:'auto'}}>
                     <HelperOfBalance />
@@ -49,28 +69,14 @@ class Home extends React.Component {
               </div>
             </div>
           </div>
-          <div className="col-auto d-flex flex-column mlpanel mtpanel">
-            <div className="row no-gutters ml0 mr0" style={{flex:'1'}}>
-              <div className="col-auto" style={{height:'100%'}}>
-                <PanelWrapper style={{height:'100%',paddingBottom:'0.7rem',width:'25rem'}} className="">
-                  <PanelHeader title={intl.get('pc_panels.order_book')}  />
-                  <HelperOfDepth />
-                </PanelWrapper>
-              </div>
-              <div className="col-auto" style={{height:'100%'}}>
-                <PanelWrapper style={{height:'100%',paddingBottom:'0.5rem',width:'25rem'}} className="mlpanel">
-                  <PanelHeader title={intl.get('pc_panels.trade_history')} />
-                  <ListMarketFills />
-                </PanelWrapper>
-              </div>
-            </div>
-            <PanelWrapper className="mtpanel" style={{height:'320px'}}>
+          <div className="d-flex flex-column mlpanel">
+            <PanelWrapper className="">
               <PanelHeader title={intl.get('pc_panels.place_order')} />
-              <div className="row no-gutters ml0 mr0 mtpanel">
+              <div className="row no-gutters ml0 mr0">
                 <div className="col-auto" style={{width:'25rem'}}>
                   <PlaceOrderFormNoSide side="buy" location={location} match={match} />
                 </div>
-                <div className="col-6" style={{width:'25rem'}}>
+                <div className="col-auto mlpanel" style={{width:'25rem'}}>
                   <PlaceOrderFormNoSide side="sell" location={location} match={match} />
                 </div>
               </div>
