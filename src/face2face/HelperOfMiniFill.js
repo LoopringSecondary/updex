7e955f1bc666b4556acb090e961b661d4ecfe407
lyd <lyddream@126.com>
2018-11-07 11:21:56 +0800

    fix mini fill

--- src/face2face/HelperOfMiniFill.js
+++ src/face2face/HelperOfMiniFill.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import {Icon, NavBar, PickerView,Stepper} from 'antd-mobile';
+import {Input} from 'antd'
 import {connect} from 'dva'
 import intl from 'react-intl-universal'
+import { toNumber } from 'LoopringJS/common/formatter'
 
 class HelperOfMiniFill extends React.Component {
   render() {
@@ -16,11 +18,23 @@ class HelperOfMiniFill extends React.Component {
       })
     }
 
-    const countChange = (value) => {
-      dispatch({type:'p2pOrder/countChange',payload:{count:value}})
+    const countChange = (e) => {
+      const { value } = e.target;
+      const reg = /^(0|[1-9][0-9]*)(\.[0-9]*)?$/;
+      if ((!isNaN(value) && reg.test(value)) || value === '' || value === '-') {
+        dispatch({type:'p2pOrder/countChange',payload:{count:value}})
+      }
     }
 
-    const data = Array.from(Array(50), (v,k) =>{return {label:k+1,value:k+1}});
+    const plus = () => {
+      dispatch({type:'p2pOrder/countChange',payload:{count:toNumber(count) +1}})
+    }
+
+    const minus = ()=>{
+      if(count >= 1){
+        dispatch({type:'p2pOrder/countChange',payload:{count:toNumber(count) -1}})
+      }
+    };
     return (
       <div className="bg-white" style={{height:'100%'}}>
         <NavBar
@@ -34,14 +48,19 @@ class HelperOfMiniFill extends React.Component {
           <div className="color-black-1">{intl.get('p2p_order.set_count')}</div>
         </NavBar>
         <div className="zb-b-b mt10 p15 pt30 center-center" style={{height:'15rem'}}>
-          <Stepper
-            showNumber
-            className="bg-white-light circle text-primary"
-            style={{margin:'0 auto'}}
-            min={1}
-            defaultValue={1}
-            value={count}
-            onChange={countChange}
+          {/*<Stepper*/}
+            {/*showNumber*/}
+            {/*className="bg-white-light circle text-primary"*/}
+            {/*style={{margin:'0 auto'}}*/}
+            {/*min={1}*/}
+            {/*defaultValue={1}*/}
+            {/*value={count}*/}
+            {/*onChange={countChange}*/}
+          {/*/>*/}
+          <Input
+                  value={count} onChange={countChange}
+                  addonBefore={<div onClick={minus}><Icon type="minus-square" theme="filled" /></div>}
+                  addonAfter={<div onClick={plus}><Icon type="plus-square" theme="filled" /></div>}
           />
         </div>
       </div>
