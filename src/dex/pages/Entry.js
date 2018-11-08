6f6b171da1635f39a6bacbefcb2c39398f60b631
dyfvicture <fbi-ceo@163.com>
2018-11-08 14:56:50 +0800

    sign

--- src/dex/pages/Entry.js
+++ src/dex/pages/Entry.js
@@ -17,31 +17,30 @@ class Entry extends React.Component {
     const {pathname} = location;
 
     const scan = ()=>{
-      // TODO test
-      const unsigned = [{"type":"order","data":{"owner":"0xebA7136A36DA0F5e16c6bDBC739c716Bb5B65a00","delegateAddress":"0x17233e07c67d086464fD408148c3ABB56245FA64","protocol":"0x8d8812b72d1e4ffCeC158D25f56748b7d67c1e78","tokenB":"0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2","tokenS":"0xef68e7c694f40c8202821edf525de3782458639f","amountB":"0x10a741a462780000","amountS":"0xa688906bd8b00000","lrcFee":"0x41c7dce2eb5a0000","validSince":"0x5be3cea8","validUntil":"0x5be52028","marginSplitPercentage":50,"buyNoMoreThanAmountB":false,"walletAddress":"0x56447c02767ba621f103c0f3dbf564dbcacf284b","orderType":"market_order","authAddr":"0x26b8ba9b1a1c31bd62415ac0e830742f2b003a18","authPrivateKey":"a92f9b5153450426399f2287cec258a9994c3669d30fe35cbd6f240fd13f763d"}}]
-      dispatch({type:'sign/unsigned',payload:{unsigned}})
-      showLayer({id:'signMessages'})
-
-      // scanQRCode().then(qrcode => {
-      //   const code = JSON.parse(qrcode)
-      //   switch(code.type) {
-      //     case 'UUID': // UUID
-      //       // updateScanLogin(owner, uuid, r, s, v, timstamp)
-      //       break;
-      //     case 'sign': // {"type":"sign","value":"0xd8e0bdf4441020e332bc56255e3c19242c4b838be3efd01be64048f083a625de"} "[{"type":"order","data":{"owner":"0xebA7136A36DA0F5e16c6bDBC739c716Bb5B65a00","delegateAddress":"0x17233e07c67d086464fD408148c3ABB56245FA64","protocol":"0x8d8812b72d1e4ffCeC158D25f56748b7d67c1e78","tokenB":"0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2","tokenS":"0xef68e7c694f40c8202821edf525de3782458639f","amountB":"0x10a741a462780000","amountS":"0xa688906bd8b00000","lrcFee":"0x41c7dce2eb5a0000","validSince":"0x5be3cea8","validUntil":"0x5be52028","marginSplitPercentage":50,"buyNoMoreThanAmountB":false,"walletAddress":"0x56447c02767ba621f103c0f3dbf564dbcacf284b","orderType":"market_order","authAddr":"0x26b8ba9b1a1c31bd62415ac0e830742f2b003a18","authPrivateKey":"a92f9b5153450426399f2287cec258a9994c3669d30fe35cbd6f240fd13f763d"}}]"
-      //     case 'cancelOrder': // original order
-      //     case 'convert': // {tx: '', owner: ''}
-      //       // getTempStore(hash)
-      //       break;
-      //     case 'P2P': // {"type":"P2P","value":{"auth":"4f66f318a5bccf372309abcfdef7166e7a35ea61627f5f80454f8d1a563a7f52","hash":"0x0d0ab8aa96b7637e0caede417be2c1dcdc328545086165a61daa9aebe98e8080","count":1}}
-      //       // getOrderByHash
-      //       break;
-      //     default:
-      //       throw new Error(`Unsupported type:${code.type}`)
-      //   }
-      // }).catch(e => {
-      //   // TODO notify
-      // })
+      scanQRCode().then(qrcode => {
+        const code = JSON.parse(qrcode)
+        switch(code.type) {
+          case 'UUID': // UUID
+            // updateScanLogin(owner, uuid, r, s, v, timstamp)
+            break;
+          case 'sign': // {"type":"sign","value":"0xd8e0bdf4441020e332bc56255e3c19242c4b838be3efd01be64048f083a625de"} "[{"type":"order","data":{"owner":"0xebA7136A36DA0F5e16c6bDBC739c716Bb5B65a00","delegateAddress":"0x17233e07c67d086464fD408148c3ABB56245FA64","protocol":"0x8d8812b72d1e4ffCeC158D25f56748b7d67c1e78","tokenB":"0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2","tokenS":"0xef68e7c694f40c8202821edf525de3782458639f","amountB":"0x10a741a462780000","amountS":"0xa688906bd8b00000","lrcFee":"0x41c7dce2eb5a0000","validSince":"0x5be3cea8","validUntil":"0x5be52028","marginSplitPercentage":50,"buyNoMoreThanAmountB":false,"walletAddress":"0x56447c02767ba621f103c0f3dbf564dbcacf284b","orderType":"market_order","authAddr":"0x26b8ba9b1a1c31bd62415ac0e830742f2b003a18","authPrivateKey":"a92f9b5153450426399f2287cec258a9994c3669d30fe35cbd6f240fd13f763d"}}]"
+          case 'cancelOrder': // original order
+          case 'convert': // {tx: '', owner: ''}
+            // getTempStore(hash)
+            //TODO TEST
+            const unsigned = [{"type":"order","data":{"owner":"0xebA7136A36DA0F5e16c6bDBC739c716Bb5B65a00","delegateAddress":"0x17233e07c67d086464fD408148c3ABB56245FA64","protocol":"0x8d8812b72d1e4ffCeC158D25f56748b7d67c1e78","tokenB":"0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2","tokenS":"0xef68e7c694f40c8202821edf525de3782458639f","amountB":"0x10a741a462780000","amountS":"0xa688906bd8b00000","lrcFee":"0x41c7dce2eb5a0000","validSince":"0x5be3cea8","validUntil":"0x5be52028","marginSplitPercentage":50,"buyNoMoreThanAmountB":false,"walletAddress":"0x56447c02767ba621f103c0f3dbf564dbcacf284b","orderType":"market_order","authAddr":"0x26b8ba9b1a1c31bd62415ac0e830742f2b003a18","authPrivateKey":"a92f9b5153450426399f2287cec258a9994c3669d30fe35cbd6f240fd13f763d"}}]
+            dispatch({type:'sign/unsigned',payload:{unsigned}})
+            showLayer({id:'signMessages'})
+            break;
+          case 'P2P': // {"type":"P2P","value":{"auth":"4f66f318a5bccf372309abcfdef7166e7a35ea61627f5f80454f8d1a563a7f52","hash":"0x0d0ab8aa96b7637e0caede417be2c1dcdc328545086165a61daa9aebe98e8080","count":1}}
+            // getOrderByHash
+            break;
+          default:
+            throw new Error(`Unsupported type:${code.type}`)
+        }
+      }).catch(e => {
+        // TODO notify
+      })
     }
     const showLayer = (payload = {}) => {
       dispatch({
