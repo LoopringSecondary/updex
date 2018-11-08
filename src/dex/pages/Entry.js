9c41dc5c428f5d57ba3bb1031f406a6c0a292917
dyfvicture <fbi-ceo@163.com>
2018-11-08 19:01:53 +0800

    sign

--- src/dex/pages/Entry.js
+++ src/dex/pages/Entry.js
@@ -11,10 +11,6 @@ import Notification from 'LoopringUI/components/Notification'
 
 class Entry extends React.Component {
 
-  state = {
-    test : ''
-  }
-
   constructor(props) {
     super(props);
   }
@@ -65,13 +61,10 @@ class Entry extends React.Component {
                   break;
                 case 'convert': // {tx: '', owner: ''}
                   unsigned = [{type:'convert', data:JSON.parse(resp.result).tx}]
-                  this.setState({test:JSON.stringify(unsigned[0].data)})
                   break;
                 default:
                   throw `Unsupported type:${code.type}`
               }
-              //TODO TEST
-              // unsigned = [{"type":"order","data":{"owner":"0xebA7136A36DA0F5e16c6bDBC739c716Bb5B65a00","delegateAddress":"0x17233e07c67d086464fD408148c3ABB56245FA64","protocol":"0x8d8812b72d1e4ffCeC158D25f56748b7d67c1e78","tokenB":"0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2","tokenS":"0xef68e7c694f40c8202821edf525de3782458639f","amountB":"0x10a741a462780000","amountS":"0xa688906bd8b00000","lrcFee":"0x41c7dce2eb5a0000","validSince":"0x5be3cea8","validUntil":"0x5be52028","marginSplitPercentage":50,"buyNoMoreThanAmountB":false,"walletAddress":"0x56447c02767ba621f103c0f3dbf564dbcacf284b","orderType":"market_order","authAddr":"0x26b8ba9b1a1c31bd62415ac0e830742f2b003a18","authPrivateKey":"a92f9b5153450426399f2287cec258a9994c3669d30fe35cbd6f240fd13f763d"}}]
               dispatch({type:'sign/unsigned',payload:{unsigned}})
               showLayer({id:'signMessages'})
             }).catch(e=>{
@@ -98,7 +91,6 @@ class Entry extends React.Component {
     }
     return (
       <div className="d-flex align-items-center justfiy-content-center" style={{height:'100vh'}}>
-        {this.state.test}
         <div className="text-center w-100 p15">
           <Button onClick={routeActions.gotoPath.bind(this,'/dex/placeOrder')} className="d-block w-100 mt15" type="primary"> Go To Market Trade </Button>
           <Button onClick={routeActions.gotoPath.bind(this,'/face2face')} className="d-block w-100 mt15" type="primary"> Go To P2P Trade </Button>
